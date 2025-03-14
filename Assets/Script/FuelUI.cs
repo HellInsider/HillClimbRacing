c303b9a06fab406643bd5da561ce9d50a3174071
STRIKER2826K <solievalik19@gmail.com>
2025-03-14 14:40:22 +0300

    Basic-mechanics-of-the-object-and-Fuel
     Добавлено  базовая механика объекта и реализована потеря топлива и его восполнение

---
+++ Assets/Script/FuelUI.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelUI : MonoBehaviour
+{
+    public Oil carFuelSystem; 
+    public Slider fuelSlider; 
+
+    void Update()
+    {
+        if (carFuelSystem != null && fuelSlider != null)
+        {
+            fuelSlider.value = carFuelSystem.GetCurrentFuel() / carFuelSystem.GetMaxFuel();
+        }
+    }
+}
